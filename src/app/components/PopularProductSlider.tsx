bf37eca862b297059b31f5ccdfae62e95cb18be4
mh <gayway1245@gamil.com>
2025-08-31 16:45:49 +0330

    product-one

--- src/app/components/PopularProductSlider.tsx
+++ src/app/components/PopularProductSlider.tsx
@@ -10,6 +10,7 @@ import "swiper/css/navigation";
 import styles from '@/app/css-module/SwiperComponent2.module.css';
 // import required modules
 import { Navigation } from 'swiper/modules';
+import Card from '@/app/components/Card';
 
 export default function PopularProductSlider() {
     return (
@@ -51,35 +52,10 @@ export default function PopularProductSlider() {
                 }}
             // className="mySwiper"
             >
-                {Array(9).fill(1).map((_, index) => <SwiperSlide>
-                    <div className="w-full group p-4 relative cursor-pointer">
-                        <div className='actions top-4'>
-                            <img src="./icons/shoping-heart-fill.svg" alt="لایک" className='absolute hidden' />
-                            <img src="./icons/shoping-heart-outline.svg" alt="لایک" className='absolute w-5 h-5 right-4 md:-right-8 md:group-hover:right-4 transition-all duration-300 ease-in-out' />
-                            <img src="./icons/shoping-cart-fill.svg" alt="سفارش" className='absolute hidden' />
-                            <img src="./icons/shopping-cart-outline.svg" alt="سفارش" className='absolute w-5 h-5 left-4 md:-left-8 md:group-hover:left-4 transition-all duration-300 ease-in-out' />
-                        </div>
-                        <div className='flex justify-center mb-2'>
-                            <img src="./images/image-popular-product.svg" alt={`پرفروش ترین محصول ${index}`} className='w-[208px] h-[208px]' />
-                        </div>
-                        <div>
-                            <h3 className="text-xl text-primary mb-2 font-black">آرد نخودچی</h3>
-                            <div className='mb-2 flex justify-between items-center'>
-                                <div className='bg-[#C62020] rounded-2xl flex items-center px-2 py-0.5'>
-                                    <span className=" text-white text-xs font-black">۶۸٪</span>
-                                </div>
-                                <div className='flex items-center'>
-                                    <span className="text-base md:text-xl text-[#353535]">۹,۳۸۳,۰۰۰</span>
-                                    <img src="./icons/tooman.svg" alt="تومان" className='w-[23px] h-[16px] my-1.5 mr-1 md:mr-2' />
-                                </div>
-                            </div>
-                            <div className='flex justify-between py-[1px]'>
-                                <span><img src="./icons/star.svg" alt="ستاره" className='inline -mt-0.5 ml-1 w-5 h-5' /><span className='text-sm'>۴.۳</span></span>
-                                <span className="text-gray-400 line-through text-sm">۹,۳۸۳,۰۰۰</span>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>)}
+                {Array(9).fill(1).map((_, index) =>
+                    <SwiperSlide>
+                        <Card index={index} />
+                    </SwiperSlide>)}
             </Swiper>
             <div className="button-slider prev-slider-2">
                 <img src="./images/arrow-right-slider.svg" alt="ناوبری راست" className="lg:w-[24px] lg:h-[24px]" />
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,20 +1,38 @@
-import Container from "@/components/Container"
+import CardGlobal from "@/components/CardGlobal"
 import { Metadata } from "next"
 
-
-
-
 export const metadata: Metadata = {
-  title: "محصولات",
+  // title: "محصولات",
 }
-
-
 
 function prodcuts() {
   return (
-    <Container>
-      <div>prodcuts</div>
-    </Container>
+    <div className="mt-20 md:mt-[131px] mb-40">
+      <aside></aside>
+      <main>
+        <div className="top mb-2 gap-4 flex flex-col">
+          <div className="search relative border-custom rounded-2xl">
+            <input type="text" placeholder="جستجو.." className="h-10 pr-[11px] w-full pl-10 text-[#353535] placeholder:text-[#ADADAD]" />
+            <img src="./images/search-normal.svg" alt="جست و جو.." className="absolute top-2 left-2" />
+          </div>
+          <div className="fillters flex">
+            <span className="py-1.5 px-3 flex gap-2">
+              <img src="./icons/filter.svg" alt="فیلتر" />
+              <span className="text-primary text-sm">فیلترها</span>
+            </span>
+            <span className="py-1.5 px-3 flex gap-2">
+              <img src="./icons/sort.svg" alt="مرتب" />
+              <span className="text-primary text-sm">مرتب سازی</span>
+            </span>
+          </div>
+        </div>
+        <div className="products grid gap-2 product-layout">
+          {Array(20).fill(1).map((_, index) =>
+            <CardGlobal key={index} />
+          )}
+        </div>
+      </main>
+    </div>
   )
 }
 
