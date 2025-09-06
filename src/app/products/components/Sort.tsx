e1a7399cb51a3fd264ece49379be6f4fbcde3e4b
mh <gayway1245@gamil.com>
2025-09-06 17:11:55 +0330

    product-four(sort)

---
+++ src/app/products/components/Sort.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useState } from "react"
+
+const Sort = () => {
+    const [handleOpneSort, setHandleOpneSort] = useState<boolean>(false);
+
+    return (
+        <>
+            <span className="py-1.5 px-3 flex gap-2 cover-shadow-action-product items-center cursor-pointer md:hidden" onClick={() => setHandleOpneSort(true)}>
+                <img src="./icons/sort.svg" alt="مرتب" className="shadow-action-product" />
+                <span className="text-primary text-sm shadow-action-product">مرتب سازی</span>
+            </span>
+            <div>
+                <div className={`cover-filter z-10 fixed top-0 right-0 left-0 bottom-0 bg-[#00000048] ${handleOpneSort ? "block" : "hidden"}`} onClick={() => setHandleOpneSort(false)}></div>
+
+                <div className={`z-10 fixed md:static bg-white md:bg-transparent w-full right-0 left-0 p-4 pb-[35px] md:p-0 transition-[bottom] duration-300 ease ${handleOpneSort ? "bottom-0" : "-bottom-[327px]"}`}>
+                    <img src="./icons/remove-gray.svg" alt="برداشتن" className="mb-4 md:hidden" onClick={() => setHandleOpneSort(false)} />
+                    <div className="flex flex-col md:flex-row md:items-center gap-y-4 md:gap-x-2">
+                        <div className="flex gap-x-2 items-center">
+                            <img src="./icons/sort.svg" alt="مرتب" className="w-6 h-6 cursor-pointer" />
+                            <span className="text-primary text-xl font-black">مرتب سازی</span>
+                        </div>
+                        <div className="flex flex-col md:flex-row items-center gap-2">
+                            <button className="button-sort-product button-sort-product-active">پربازدیدترین</button>
+                            <button className="button-sort-product">پرفروش‌ترین</button>
+                            <button className="button-sort-product">محبوب‌‌ترین</button>
+                            <button className="button-sort-product">ارزان‌ترین</button>
+                            <button className="button-sort-product">گران‌ترین</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Sort
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,7 @@
 import CardGlobal from "@/components/CardGlobal"
 import { Metadata } from "next"
+import Sort from "./components/Sort"
+import Filter from "./components/Filter"
 
 export const metadata: Metadata = {
   title: "محصولات",
@@ -8,22 +10,21 @@ export const metadata: Metadata = {
 function prodcuts() {
   return (
     <div className="mt-20 md:mt-[131px] mb-56 md:mb-16">
-      <aside></aside>
+      <aside className="bg-blue-500 w-full z-10 flex flex-col fixed bottom-0 right-0 left-0">
+        {/* <Filter /> */}
+      </aside>
       <main>
-        <div className="top mb-2 gap-4 flex flex-col">
-          <div className="search relative border-custom rounded-2xl">
+        <div className="top mb-2 md:mb-4 gap-4 flex flex-col">
+          <div className="search relative border-custom rounded-2xl bg-white">
             <input type="text" placeholder="جستجو.." className="h-10 pr-[11px] w-full pl-10 text-[#353535] placeholder:text-[#ADADAD]" />
             <img src="./images/search-normal.svg" alt="جست و جو.." className="absolute top-2 left-2" />
           </div>
-          <div className="fillters flex">
-            <span className="py-1.5 px-3 flex gap-2 cover-shadow-action-product cursor-pointer">
-              <img src="./icons/filter.svg" alt="فیلتر" className="shadow-action-product" />
-              <span className="text-primary text-sm shadow-action-product">فیلترها</span>
+          <div className="fillters flex select-none items-center md:justify-center">
+            <span className="py-1.5 px-3 flex gap-2 cover-shadow-action-product cursor-pointer items-center md:hidden">
+              <img src="./icons/filter.svg" alt="فیلتر" className="shadow-action-product w-5 h-5 md:h-6 md:w-6" />
+              <span className="text-primary text-sm shadow-action-product md:text-xl md:font-black">فیلترها</span>
             </span>
-            <span className="py-1.5 px-3 flex gap-2 cover-shadow-action-product cursor-pointer">
-              <img src="./icons/sort.svg" alt="مرتب" className="shadow-action-product" />
-              <span className="text-primary text-sm shadow-action-product">مرتب سازی</span>
-            </span>
+            <Sort/>
           </div>
         </div>
         <div className="flex flex-wrap justify-center gap-2 md:gap-4 product-layout mb-4 lg:mb-8">
